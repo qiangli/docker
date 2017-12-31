01feb36d176aa0bb88a60c413c26eed1e0d53fd4
Qiang Li <liqiang@gmail.com>
2017-12-31 13:47:49 -0800

    Create wsl-docker-env.sh

---
+++ wsl-docker-env.sh
@@ -0,0 +1,7 @@
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_MACHINE_NAME=default
+export DOCKER_TLS_VERIFY=1
+export DOCKER_TOOLBOX_INSTALL_PATH="/mnt/C/Program Files/Docker Toolbox"
+export DOCKER_CERT_PATH="/mnt/c/Users/liqiang/.docker/machine/machines/default"
+#
+alias docker-machine=docker-machine.exe
